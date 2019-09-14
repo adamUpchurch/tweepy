ffd9afa41491f49be49cf43a21d9eb40a9401a69
Adam Upchurch <aupchurch167@gmail.com>
2019-09-14 02:52:43 -0400

    wtf - I hate deploying apps

--- app.py
+++ app.py
@@ -2,42 +2,43 @@
 from flask import Flask, request, render_template, jsonify
 import json
 from twitterAPI  import followPeople, create_friendship, create_favorite, retweet, get_tweets, reply_to_tweet
-application = Flask(__name__)
+app = Flask(__name__)
 
-@application.route('/find_tweets_by_phrase', methods=['POST'])
+@app.route('/', methods=['GET'])
+def index():
+    return jsonify({"hey":"Tweepy API for Hey Cindy!"})
+
+@app.route('/find_tweets_by_phrase', methods=['POST'])
 def getting_some_sweet_tweets():
     data = json.loads(request.data)
     return jsonify(get_tweets(data['phrase'], data['twitterCredentials']))
 
-@application.route('/create_friendship', methods=['POST'])
+@app.route('/create_friendship', methods=['POST'])
 def follow_new_friend():
     data = json.loads(request.data)
     return create_friendship(data['follower_id'], data['twitterCredentials'])
 
-@application.route('/create_favorite', methods=['POST'])
+@app.route('/create_favorite', methods=['POST'])
 def fav_a_tweet():
     data = json.loads(request.data)
     return create_favorite(data['tweet_id'], data['twitterCredentials'])
 
-@application.route('/create_retweet', methods=['POST'])
+@app.route('/create_retweet', methods=['POST'])
 def re_tweet():
     data = json.loads(request.data)
     return retweet(data['tweet_id'], data['twitterCredentials'])
 
-@application.route('/reply_to_tweet', methods=['POST'])
+@app.route('/reply_to_tweet', methods=['POST'])
 def reply_tweet():
     data = json.loads(request.data)
     return reply_to_tweet(data['tweet_id'], data['status'], data['twitterCredentials'])
 
-@application.route('/follow_random', methods=['POST'])
+@app.route('/follow_random', methods=['POST'])
 def follow_randos():
     data = json.loads(request.data)
     return jsonify(followPeople(thatSaid=data['thatSaid'], credentials= data['twitterCredentials'], atMost=data['atMost'], polarityMin=data['polarityMin']))
 
-@application.route('/', methods=['GET'])
-def index():
-    return "Tweepy API for Hey Cindy!"
 
 if __name__ == "__main__":
-    application.debug = True
-    application.run()
+    app.debug = True
+    app.run()
